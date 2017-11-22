30b0bc1dd3cb951d56a793ecf5538f6a115dff4b
Ryan Gifford <rgifford@solutionreach.com>
2017-11-22 12:56:38 -0700

    Hipchat Test

--- vars/deployGen.groovy
+++ vars/deployGen.groovy
@@ -9,12 +9,12 @@ def call(data, env) {
   service_name = data.env.SERVICE_NAME
   repo_name = data.env.REPO_NAME
   tag_id = data.GIT_COMMIT
-  sh 'printenv'
+//  sh 'printenv'
   echo 'Deploying to pants'
   sh "sed -i.bak 's/\${build_number}/${tag_id}/g' deployment.yml"
   sh "sed -i.bak 's/\${image_name}/${service_name}/g' deployment.yml"
   sh "kubectl config use-context ${env}"
   sh 'kubectl apply --record=true -f deployment.yml'
   sh "kubectl rollout status deployment ${service_name}"
-  hipchatSend (color: 'YELLOW', credentialId: 'rgifford-hipchat-api-send-only2', failOnError: false, message: "Just deployed ${service_name}:${tag_id} to ${env}. Thought you should know.  ${data.BLUE_OCEAN_URL}", notify: true, room: '4018492', sendAs: 'Jenkins', server: 'api.hipchat.com', textFormat: true, v2enabled: true)
+  hipchatSend (color: 'YELLOW', credentialId: 'rgifford-hipchat-api-send-only2', failOnError: false, message: "Just deployed ${service_name}:${tag_id} to ${env}. Thought you should know.", notify: true, room: '4018492', sendAs: 'Jenkins', server: 'api.hipchat.com', textFormat: true, v2enabled: true)
 }
