593172dfc8e82f181422e2a5f98a7663de96f4b7
Ryan Gifford <rgifford@solutionreach.com>
2017-11-22 11:38:52 -0700

    Add docker push library

---
+++ vars/dockerPush.groovy
@@ -0,0 +1,14 @@
+#!/usr/bin/env groovy
+
+/**
+ * Build Docker images in a pipeline
+ */
+
+def call(data) {
+  echo "PUSHING DOCKER IMAGE"
+  service_name = data.env.SERVICE_NAME
+  repo_name = data.env.REPO_NAME
+  tag_id = data.GIT_COMMIT
+//  sh 'printenv'
+  sh "gcloud docker -- push ${repo_name}/${service_name}:${tag_id}"
+}
