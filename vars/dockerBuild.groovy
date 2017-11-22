93ba02fc67f6c2d2f1334d3a88c47f24cf4edcd1
Ryan Gifford <rgifford@solutionreach.com>
2017-11-22 10:57:49 -0700

    this

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -5,7 +5,7 @@
  */
 
 def call(data) {
-  service_name = ${data.env.SERVICE_NAME}
+  service_name = data.env.SERVICE_NAME
   echo "SERVICE_NAME: ${service_name}"
   sh 'printenv'
 //  sh 'docker build -t ${image} .'
