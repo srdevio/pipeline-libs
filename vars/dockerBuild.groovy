a749fcb00d0276f3ddf316762af2007427969db9
Ryan Gifford <rgifford@solutionreach.com>
2017-11-22 11:09:01 -0700

    build yo

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -8,9 +8,6 @@ def call(data) {
   service_name = data.env.SERVICE_NAME
   repo_name = data.env.REPO_NAME
   tag_id = data.GIT_COMMIT
-  echo "SERVICE_NAME: ${service_name}"
-  echo "REPO_NAME: ${repo_name}"
-  echo "TAG_ID: ${tag_id}"
 //  sh 'printenv'
   sh "docker build -t ${repo_name}/${service_name}:${tag_id} ."
 }
